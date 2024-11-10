8db6a0d0eaec555525031b2c47bdc1313d6f9cc7
Spencer Hill <spencerthefencer10@hotmail.com>
2024-11-09 23:25:52 -0700

    Did some balancing for the game.

--- src/play/play.jsx
+++ src/play/play.jsx
@@ -9,18 +9,23 @@ export function Play() {
   const [score, setScore] = useState(0);
   const [playerPosition, setPlayerPosition] = useState(4);
   const [squares, setSquares] = useState(Array(9).fill({type: null, growth: 0}));
+  const [intervalCount, setIntervalCount] = useState(0);
+  let intervalAmount = 2000;
+  let intervalMin = 200;
 
   function reset() {
     setGridSize(3);
     setScore(0);
     setPlayerPosition(4);
     setSquares(Array(9).fill({type: null, growth: 0}));
+    setIntervalCount(0);
   }
 
   function increaseGridSize() {
     console.log("increasing grid size!");
     setGridSize(gridSize + 2);
     setSquares(Array((gridSize + 2) * (gridSize + 2)).fill({type: null, growth: 0}));
+    intervalAmount += 200;
   }
 
   function increaseScore() {
@@ -61,7 +66,7 @@ export function Play() {
         default:
           break;
       }
-      if (squares[newPosition].type === 'square') {
+      if (squares[newPosition].type === 'square' && squares[newPosition].growth < 100) {
         increaseScore();
         setSquares(squares.map((square, index) => index === newPosition ? {type: null, growth: 0} : square));
       }
@@ -77,22 +82,23 @@ export function Play() {
 
   useEffect(() => {
     const interval = setInterval(() => {
+      setIntervalCount(intervalCount + 1);
       const newSquares = squares.slice();
       let emptyIndices = newSquares.map((square, index) => square.type === null ? index : null).filter(index => index !== null);
       if (emptyIndices.length > 0) {
         const randomIndex = emptyIndices[Math.floor(Math.random() * emptyIndices.length)];
         newSquares[randomIndex] = { type: 'square', growth: 0 };
       }
-      setSquares(newSquares.map(square => square.type === 'square' && square.growth < 100 ? { ...square, growth: square.growth + 10 } : square));
-    }, 2000);
+      setSquares(newSquares.map(square => square.type === 'square' && square.growth < 100 ? { ...square, growth: square.growth + 5 } : square));
+      if (gridSize > 3) { intervalMin += 200; }
+    }, Math.max(intervalMin, intervalAmount - intervalCount * 50));
     return () => clearInterval(interval);
-  }, [squares]);
+  }, [squares, intervalCount]);
 
   return (
     <main className="container-fluid text-center">
       <Button onClick={reset}>Reset</Button>
-      <Button onClick={increaseGridSize}>Size</Button>
-      <Button onClick={increaseScore}>Score</Button>
+      <div>Controls: W A S D</div>
       <div>Score: {score}</div>
       <Grid gridSize={gridSize} playerLocation={playerPosition} squares={squares} />
     </main>
