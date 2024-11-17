2e699068032a7ed82c35f91356c0d3255e7d6c45
Spencer Hill <spencerthefencer10@hotmail.com>
2024-11-16 23:13:21 -0700

    Added 3rd party api: random jokes

--- src/profile/profile.jsx
+++ src/profile/profile.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './profile.css';
 
-export function Profile({userName, highScore}) {
+export function Profile({ userName, highScore }) {
+  const [joke, setJoke] = useState('');
+
+  useEffect(() => {
+    async function fetchJoke() {
+      try {
+        const response = await fetch('https://official-joke-api.appspot.com/random_joke');
+        const data = await response.json();
+        setJoke(`${data.setup} - ${data.punchline}`);
+      } catch (error) {
+        console.error('Error fetching joke:', error);
+        setJoke('Could not fetch a joke at this time.');
+      }
+    }
+    fetchJoke();
+  }, []);
+
   return (
     <main className="container-fluid text-center" id='profile'>
-        <div>Username: {userName}</div>
-        <div>High Score: {highScore}</div>
+      <div>Username: {userName}</div>
+      <div>High Score: {highScore}</div>
+      <br />
+      <div>Joke: {joke}</div>
     </main>
   );
 }
