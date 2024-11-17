446f0fa96acac96d7913f97d6937b2c80fe226d5
Spencer Hill <spencerthefencer10@hotmail.com>
2024-11-16 17:08:50 -0700

    Finished implementing index.js

--- service/index.js
+++ service/index.js
@@ -7,12 +7,77 @@ let scores = [];
 
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
+app.use(express.json());
 
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
 
-app.get('*', (req, res) => {
-    res.send({msg:'Block Bonanza service'});
+apiRouter.post('/auth/create', async (req, res) => {
+    const user = users[req.body.username];
+    if (user) {
+        res.status(409).send({msg:'Existing user'});
+    } else {
+        const user = {username: req.body.username, password: req.body.password, token: uuid.v4()};
+        user[user.username] = user;
+
+        res.send({token: user.token});
+    }
+});
+
+apiRouter.post('/auth/login', async (req, res) => {
+    const user = users[req.body.username];
+    if (user) {
+        if (req.body.password === user.password) {
+            user.token = uuid.v4();
+            res.send({token: user.token});
+            return;
+        }
+    }
+    res.status(401).send({msg: 'Unauthorized'});
+});
+
+apiRouter.delete('/auth/logout', (req, res) => {
+    const user = Object.values(users).find((u) => u.token === req.body.token);
+    if (user) {
+        delete user.token;
+    }
+    res.status(204).end();
+});
+
+apiRouter.get('/scores', (req, res) => {
+    res.send(scores);
+});
+
+apiRouter.post('/score', (req, res) => {
+    scores = updateScores(req.body, scores);
+    res.send(scores);
+});
+
+app.use((req, res) => {
+    res.sendFile('index.html', {root: 'public'});
 });
 
 app.listen(port, () => {
-    console.log(`LIsting on port ${port}`);
+    console.log(`Listening on port ${port}`);
 });
+
+function updateScores(newScore, scores) {
+    let found = false;
+    for (const [i, prevScore] of scores.entries()) {
+        if (newScore.score > prevScore.score) {
+            scores.splice(i, 0, newScore);
+            found = true;
+            break;
+        }
+    }
+
+    if (!found) {
+        scores.push(newScore);
+    }
+
+    if (scores.length > 10) {
+        scores.length = 10;
+    }
+
+    return scores;
+}
