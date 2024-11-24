736f0816b1cdb144a078ffcfd29a32a267f2c7cc
Spencer Hill <spencerthefencer10@hotmail.com>
2024-11-23 22:34:29 -0700

    Fixed conflicts

--- service/database.js
+++ service/database.js
@@ -9,7 +9,6 @@ const db = client.db('startup');
 const userCollection = db.collection('user');
 const scoreCollection = db.collection('score');
 
-<<<<<<< HEAD
 // This will asynchronously test the connection and exit the process if it fails
 (async function testConnection() {
   await client.connect();
@@ -76,55 +75,4 @@ module.exports = {
   updateHighScore,
   addScore,
   getHighScores,
-=======
-(async function testConnection() {
-    await client.connect();
-    await db.command({ ping: 1 });
-})().catch((ex) => {
-    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
-    process.exit(1);
-});
-
-function getUser(username) {
-    return userCollection.findOne({ username: username });
-}
-
-function getUserByToken(token) {
-    return userCollection.findOne({ token: token });
-}
-
-async function createUser(username, password) {
-    const passwordHash = await bcrypt.hash(password, 10);
-
-    const user = {
-        username: username,
-        password: passwordHash,
-        token: uuid.v4(),
-    };
-    await userCollection.insertOne(user);
-
-    return user;
-}
-
-async function addScore(score) {
-    return scoreCollection.insertOne(score);
-}
-
-function getHighScores() {
-    const query = { score: { $gt: 0 } };
-    const options = {
-        sort: { score: -1 },
-        limit: 10,
-    };
-    const cursor = scoreCollection.find(query, options);
-    return cursor.toArray();
-}
-
-module.exports = {
-    getUser,
-    getUserByToken,
-    createUser,
-    addScore,
-    getHighScores,
->>>>>>> main
 };
