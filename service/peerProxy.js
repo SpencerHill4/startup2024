88e9d020d4ab8c7597746a276698d7234095f209
Spencer Hill <spencerthefencer10@hotmail.com>
2024-12-07 17:56:33 -0700

    Added ablility to upgrade to websocket connecton.

--- service/peerProxy.js
+++ service/peerProxy.js
@@ -2,5 +2,11 @@ const { WebSocketServer } = require('ws');
 const uuid = require('uuid');
 
 function peerProxy(httpServer) {
-    
+    const wss = new WebSocketServer({ noServer: true });
+
+    httpServer.on('upgrade', (request, socket, head) => {
+        wss.handleUpgrade(request, socket, head, function done(ws) {
+        wss.emit('connection', ws, request);
+        });
+    });
 }
